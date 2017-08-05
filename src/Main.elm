8b412ad35006fdb73af668d9230badfb7abfe4b7
Noah Gordon <noahzacharygordon@gmail.com>
2017-08-05 16:13:16 -0400

    First pass styling + content

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,14 @@
 module Main exposing (main)
 
 import Html
+import Color exposing (..)
 import Platform.Sub as Sub
-import Element exposing (text)
-import Style
+import Element exposing (..)
+import Element.Attributes exposing (..)
+import Style exposing (style)
+import Style.Font as Font
+import Style.Border as Border
+import Style.Color as Color
 
 
 main =
@@ -15,10 +20,66 @@ main =
         }
 
 
+type Styles
+    = Title
+    | Description
+    | DescriptionLink
+    | Footer
+    | FooterText
+    | FooterLink
+    | None
+
+
 view model =
     Element.viewport styleSheet <|
-        text "Behold, Elm NYC!"
+        column None [ height (percent 100), paddingXY 120 0 ] <|
+            [ column None [ center, verticalCenter, spacing 20, height (fill 1) ] <|
+                [ image "./images/logo.png" None [ width (px 200), height (px 200) ] empty
+                , el Title [] (text "elm-nyc")
+                , paragraph Description [] <|
+                    [ text "welcome! join us on "
+                    , text "slack" |> el DescriptionLink [ target "_blank" ] |> link "http://elmnyc.herokuapp.com/"
+                    , text " or at our "
+                    , text "meetup group" |> el DescriptionLink [ target "_blank" ] |> link "https://www.meetup.com/Elm-NYC/"
+                    , text ". learn more "
+                    , text "here" |> el DescriptionLink [ target "_blank" ] |> link "/"
+                    , text "!"
+                    ]
+                ]
+            , row Footer [ verticalCenter, center, spacing 100, height (px 75) ] <|
+                [ paragraph FooterText [] <|
+                    [ text "contact: ( "
+                    , text "kara" |> el FooterLink [] |> link "mailto:krbernet@gmail.com"
+                    , text ", "
+                    , text "noah" |> el FooterLink [] |> link "mailto:noahzacharygordon@gmail.com"
+                    , text " )"
+                    ]
+                , paragraph FooterText [] <|
+                    [ text "code of conduct" |> el FooterLink [] |> link "/" ]
+                ]
+            ]
 
 
 styleSheet =
-    Style.styleSheet []
+    Style.styleSheet
+        [ style Title
+            [ Font.size 28 ]
+        , style Description
+            [ Font.size 20 ]
+        , style DescriptionLink
+            [ Color.text black
+            , Font.bold
+            , Style.hover
+                [ Color.text (rgb 127 209 59) ]
+            ]
+        , style Footer
+            [ Border.top 2, Border.solid, Color.border (greyscale 0.3) ]
+        , style FooterText
+            [ Color.text (greyscale 0.3) ]
+        , style FooterLink
+            [ Color.text (greyscale 0.3)
+            , Font.bold
+            , Style.hover
+                [ Color.text (rgb 127 209 59) ]
+            ]
+        ]
